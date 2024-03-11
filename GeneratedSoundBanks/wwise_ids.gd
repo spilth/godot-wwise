031e7bbeeffd798d0628347c63b2efb866dc388f
Brian Kelly <polymonic@gmail.com>
2024-03-11 14:38:41 -0500

    Working dynamic Battle Music in a test scene

---
+++ GeneratedSoundBanks/wwise_ids.gd
@@ -0,0 +1,62 @@
+class_name AK
+
+class EVENTS:
+
+	const BATTLEMUSIC1 = 1879833053
+
+	const _dict = {
+		"BattleMusic1": BATTLEMUSIC1
+	}
+
+class STATES:
+
+	const _dict = {}
+
+class SWITCHES:
+
+	const _dict = {}
+
+class GAME_PARAMETERS:
+
+	const _dict = {}
+
+class TRIGGERS:
+
+	const _dict = {}
+
+class BANKS:
+
+	const INIT = 1355168291
+	const MAIN = 3161908922
+
+	const _dict = {
+		"Init": INIT,
+		"Main": MAIN
+	}
+
+class BUSSES:
+
+	const MASTER_AUDIO_BUS = 3803692087
+
+	const _dict = {
+		"Master Audio Bus": MASTER_AUDIO_BUS
+	}
+
+class AUX_BUSSES:
+
+	const _dict = {}
+
+class AUDIO_DEVICES:
+
+	const SYSTEM = 3859886410
+	const NO_OUTPUT = 2317455096
+
+	const _dict = {
+		"System": SYSTEM,
+		"No_Output": NO_OUTPUT
+	}
+
+class EXTERNAL_SOURCES:
+
+	const _dict = {}
+
